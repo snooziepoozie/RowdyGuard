ef39f3b80cb45d35caf6cbe3fc64dbaf8e253b9f
HullaBrian <beierlejc@gmail.com>
2024-11-04 21:56:20 -0600

    Started working on database stuff

--- RowdyGuard/app/src/main/java/edu/utsa/cs3443/rowdyguard/model/db/Handler.java
+++ RowdyGuard/app/src/main/java/edu/utsa/cs3443/rowdyguard/model/db/Handler.java
@@ -1,4 +1,28 @@
 package edu.utsa.cs3443.rowdyguard.model.db;
 
+import android.content.res.AssetManager;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import edu.utsa.cs3443.rowdyguard.controllers.MainActivity;
+
 public class Handler {
+    final private String database_name = "database.json";
+    private JSONObject database;
+    private AssetManager assetManager;
+
+    public Handler(MainActivity a) {
+        this.assetManager = a.getAssets();
+    }
+    public void loadDB() {
+        Scanner scanner = null;
+        try {
+            InputStream file = this.assetManager.open(this.database_name);
+            scanner = new Scanner(file);
+        } catch (IOException ignored) {}
+    }
 }
