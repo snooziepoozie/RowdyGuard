eee3903b076096db1cbb2894d0978ee8ce9578e1
Makayla <makayla.frank@my.utsa.edu>
2024-11-14 13:41:07 -0600

    login screen!

--- RowdyGuard/app/src/main/java/edu/utsa/cs3443/rowdyguard/controllers/MainActivity.java
+++ RowdyGuard/app/src/main/java/edu/utsa/cs3443/rowdyguard/controllers/MainActivity.java
@@ -2,8 +2,6 @@ package edu.utsa.cs3443.rowdyguard.controllers;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,12 +10,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import edu.utsa.cs3443.rowdyguard.R;
-import edu.utsa.cs3443.rowdyguard.model.db.Handler;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Handler handler;
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -27,57 +23,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button btn = findViewById(R.id.addButton);
-        Button btn2 = findViewById(R.id.removeButton);
-        try {
-            this.handler = new Handler("P@ssw0rd", this);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        final int[] i = {0};
-
-        // LinearLayout layout = findViewById(R.id.linearLayout);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    handler.addPassword("Password" + i[0], "username", "P@ssw0rd");
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-                i[0]++;
-            }
-        });
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    handler.removePassword(handler.getPasswords().get(handler.getPasswords().size() - 1).getTitle());
-                } catch (java.lang.IndexOutOfBoundsException e) {
-                    System.out.println("Cannot find password to delete!");
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
+        MainController controller = new MainController(this);
     }
-//    private void addVaultButton(LinearLayout layout, String text) {
-//        Button button = new Button(this);
-//        button.setText(text);
-//
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                try {
-//
-//                } catch (Exception e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }
-//        });
-//
-//        layout.addView(button);
-//    }
 }
