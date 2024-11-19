b27bb1fd18e589effe063895e1edb07ba15ad995
Jeronimp Gonzalaz <jeronimo.gonzalez@my.utsa.edu>
2024-11-19 12:12:31 -0600

    edit1

--- RowdyGuard/app/src/main/java/edu/utsa/cs3443/rowdyguard/controllers/MainActivity.java
+++ RowdyGuard/app/src/main/java/edu/utsa/cs3443/rowdyguard/controllers/MainActivity.java
@@ -1,26 +1,43 @@
 package edu.utsa.cs3443.rowdyguard.controllers;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ToggleButton;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import edu.utsa.cs3443.rowdyguard.R;
+import edu.utsa.cs3443.rowdyguard.model.Settings;
 
 
 public class MainActivity extends AppCompatActivity {
+    private Settings settings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.settings_view);
+
+        //iniatilize with default password
+        settings = new Settings("password");
+
+        // Change Password Button
+        Button changePasswordButton = findViewById(R.id.changePasswordButton);
+        changePasswordButton.setOnClickListener(v -> {
+            settings.setPassword("password1");
+            System.out.println("Password changed successfully.");
+        });
+
+        ToggleButton themeToggleButton = findViewById(R.id.themeToggleButton);
+        themeToggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            settings.toggleTheme();
+            System.out.println("Theme toggled. Dark Theme: " + settings.isDarkThemeEnabled());
+        });
+
+        // Logout Button
+        Button logoutButton = findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(v -> {
+            settings.logout();
+            finish();
         });
     }
 }
