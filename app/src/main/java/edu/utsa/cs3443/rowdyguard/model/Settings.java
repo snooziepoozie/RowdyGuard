8a873f9664a9177f7c52df15a88ab966ebed2a75
JFGonz <138318130+JFGonz@users.noreply.github.com>
2024-11-30 23:32:55 -0600

    Update Settings.java javadoc

--- app/src/main/java/edu/utsa/cs3443/rowdyguard/model/Settings.java
+++ app/src/main/java/edu/utsa/cs3443/rowdyguard/model/Settings.java
@@ -1,11 +1,18 @@
 package edu.utsa.cs3443.rowdyguard.model;
-
+/**
+ * Object represents application settings such as app theme and 
+ * app password.
+ * @author Jeronimo Gonzalez
+ */
 
 public class Settings {
     //Variables
     private boolean isDarkTheme;
 
     //Constructor
+    /**
+     * Constructor to initialize values, sets theme to light mode.
+     */
     public Settings() {
         this.isDarkTheme = false;
     }
@@ -13,10 +20,14 @@ public class Settings {
     //Getter methods
     public boolean isDarkThemeEnabled() { return isDarkTheme; }
 
-    // Setter methods
+    /** 
+    * Setter methods, toggles between light and dark mode.
+    */
     public void toggleTheme() { this.isDarkTheme = !this.isDarkTheme; }
 
-    //Logout function
+    /**
+     * Logout function.
+     */
     public void logout() { System.out.println("User logged out successfully."); }
 }
 
